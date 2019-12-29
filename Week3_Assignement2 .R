0da5c9aadd786b2ebe4435cbe3f6c76d4664cf23
sitanshu1000 <13702072+sitanshu1000@users.noreply.github.com>
2019-12-29 18:48:32 +0530

    Week 3 Assignment 2

---
+++ Week3_Assignement2 .R
@@ -0,0 +1,35 @@
+
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+
+cacheSolve <- function(x, ...) {
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+}
+
+test <- makeCacheMatrix(rbind(c(1, 2), c(3, 4)))
+test$get()
+cacheSolve(test)
+test$set(rbind(c(61, 32, 93), c(23, 5, 43), c(21, 89, 83)))
+cacheSolve(test)
+test$set(matrix(6:14, nrow = 3, ncol = 3))
+
+
